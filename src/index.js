82fa8c47048e4f538801b9b3d39e36e192390e0c
Danny Michaelis <danny@exploreplanet3.com>
2018-01-27 08:37:09 -0500

    starting server

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-let app = document.querySelector('#app')
+import pushMessage from './notifier';
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
-app.innerHTML = '<h2>Welcome to cfh-server</h2>'
+const app = express();
+app.use( bodyParser.json() );
+app.use( morgan( ':remote-addr - :remote-user :method :url :status' ) );
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+const previousPlayers = [];
+
+app.post( '/notification', req => {
+    console.log( 'new token: ' +  req.body.token );
+    previousPlayers.push( req.body.token );
+} );
+
+app.get( '/help', () => {
+    setTimeout( () => pushMessage( previousPlayers ), 30000 );
+} );
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+
+app.listen( 3000, () => console.log( 'Example app listening on port 3000!' ) );
